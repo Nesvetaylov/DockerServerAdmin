54f7d5ade63999b77da6e3e4696698b68f577e76
Vlad <vlad.nesvetaylov@mail.ru>
2025-06-27 12:27:10 +0000

    build 2.4

--- src/database.py
+++ src/database.py
@@ -1,16 +1,21 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_async_engine('postgresql+psycopg://kubsu:kubsu@127.0.0.1:5432/kubsu', echo=True)
+DATABASE_URL = os.getenv(
+"DATABASE_URL",
+"postgresql+psycopg://kubsu:kubsu@127.0.0.1:5432/kubsu&quot;
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
-
 async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
+async with engine.begin() as conn:
+await conn.run_sync(Base.metadata.create_all)
 
 async def get_db() -> AsyncSession:
-    async with AsyncSessionLocal() as session:
-        yield session
+async with AsyncSessionLocal() as session:
+yield session
